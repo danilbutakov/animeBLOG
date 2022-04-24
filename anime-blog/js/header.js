5861ee47d115e4395245d1bbcabd9b61c13b348e
danilbutakov <12021109359@irgups.ru>
2022-04-24 22:54:02 +0800

    Redone3 header.js

--- anime-blog/js/header.js
+++ anime-blog/js/header.js
@@ -1,9 +1,5 @@
 "use strict";
 
-$('.header__burger').click(function(event) {
-    $('.header__burger,.header__menu').toggleClass('active');
-    $('body').toggleClass('lock');
-});
 
 document.querySelectorAll('.header__burger').onclick = function() {
     open();
