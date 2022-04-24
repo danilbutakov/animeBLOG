826598637823ebef79cd7f9670ce97cf45a37d5c
danilbutakov <12021109359@irgups.ru>
2022-04-24 23:00:50 +0800

    returned old header.js

--- anime-blog/js/header.js
+++ anime-blog/js/header.js
@@ -1,11 +1,6 @@
 "use strict";
 
-
-document.querySelectorAll('.header__burger').onclick = function() {
-    open();
-};
-
-function open() {
-    document.querySelectorAll('.header__burger,.header__menu').toggleClass('.active');
-    document.querySelectorAll('body').toggleClass('lock');
-}
+$('.header__burger').click(function(event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+});
