6b152e45850de1c1a58e24271526d1d217e81b8a
Danil Butakov <12021109359@irgups.ru>
2022-04-24 20:16:12 +0800

    fc

---
+++ js/formIndex.js
@@ -0,0 +1,34 @@
+let form = document.querySelector('.js-form'),
+    formInputs = document.querySelectorAll('.js-input'),
+    inputEmail = document.querySelector('.js-input-email');
+
+    function validateEmail(email) {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }   
+
+
+    form.onsubmit = function () {
+        let emailVal = inputEmail.value,
+            emptyInputs = Array.from(formInputs).filter(input => input.value === '');
+
+            formInputs.forEach(function (input) {
+                if (input.value === '') {
+                    input.classList.add('error');
+                } else {
+                    input.classList.remove('error');
+                    return false;
+                }
+            });
+
+            
+
+            if(!validateEmail(emailVal)) {
+                console.log('email not valid');
+                inputEmail.classList.add('error');
+                return false;
+            } else {
+                inputEmail.classList.remove('error');
+            }    
+            
+}
