a03ad1c47bade1b753735307f389c7939d38cca8
danilbutakov <12021109359@irgups.ru>
2022-04-24 22:53:16 +0800

    Redone2 header.js

--- anime-blog/js/header.js
+++ anime-blog/js/header.js
@@ -1,11 +1,15 @@
 "use strict";
 
+$('.header__burger').click(function(event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+});
 
 document.querySelectorAll('.header__burger').onclick = function() {
     open();
 };
 
 function open() {
-    document.querySelectorAll('.header__burger, .header__menu').toggleClass('active');
+    document.querySelectorAll('.header__burger,.header__menu').toggleClass('active');
     document.querySelectorAll('body').toggleClass('lock');
 }
