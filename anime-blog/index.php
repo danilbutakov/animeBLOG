9f4e802dd20307631435c681a6b2c027658a7bb9
danilbutakov <12021109359@irgups.ru>
2022-04-28 01:05:22 +0800

    Переделал знаки в phpFunctions9

--- anime-blog/index.php
+++ anime-blog/index.php
@@ -29,35 +29,22 @@ $mysqli->set_charset('utf8');
 
     $mysqli->close();
 
-?>
 
-<?php
-    
-    $mysqli = new mysqli('localhost', 'dan', '1234', 'anime-blog');
-
-    if(mysqli_connect_errno()) {
-        printf('Соединение не установлено', mysqli_error());
-        exit();
-    }
-
-
-    $mysqli->set_charset('utf8');
-
-    $query = $mysqli->query('SELECT * FROM subscribe');
+    $query2 = $mysqli2->query('SELECT * FROM subscribe');
 
     $emailSub = $_REQUEST['email__sub'];
-    if($query != '') {
-        $query = "INSERT INTO subscribe VALUES (null, '$emailSub')";
+    if($query2 != '') {
+        $query2 = "INSERT INTO subscribe VALUES (null, '$emailSub')";
     }
 
 
-    if(mysqli_query($mysqli, $query)){
+    if(mysqli_query($mysqli2, $query2)){
         //echo "Записи успешно добавлены.";
     } else{
-        "ERROR: Не удалось выполнить $query. " . mysqli_error($mysqli);
+        "ERROR: Не удалось выполнить $query2. " . mysqli_error($mysqli2);
     }
 
-    $mysqli(close);
+    $mysqli2(close);
 
 ?>
 <!DOCTYPE html>
