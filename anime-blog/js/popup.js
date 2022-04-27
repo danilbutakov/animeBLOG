25bf899b2c0ac108ac8701cc9362a0040ca7d86f
danilbutakov <12021109359@irgups.ru>
2022-04-27 22:39:40 +0800

    Добавил кнопку закрытия поп апа21

--- anime-blog/js/popup.js
+++ anime-blog/js/popup.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const openPopUp = document.getElementById('open__pop_up');
-const closePopUp = document.getElementById('close__pop_up', 'close__pop_up_1');
+const closePopUp = document.getElementsById('close__pop_up', 'close__pop_up_1');
 const popUp = document.getElementById('pop_up');
 
 openPopUp.addEventListener('click', function(e) {
