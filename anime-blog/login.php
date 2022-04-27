6f209922f1529f5ed6d2626686f630cc031546a7
danilbutakov <12021109359@irgups.ru>
2022-04-28 00:56:58 +0800

    Переделал знаки в phpFunctions4

--- anime-blog/login.php
+++ anime-blog/login.php
@@ -1,6 +1,66 @@
 <?php
-    include_once './php/authorization.php';
-    include_once './php/registration.php';
+
+    $mysqli = new mysqli('localhost', 'dan', '1234', 'anime-blog');
+
+    if(mysqli_connect_errno()) {
+        printf('Соединение не установлено', mysqli_error());
+        exit();
+    }
+
+
+    $mysqli->set_charset('utf8');
+
+    $email = $_REQUEST['email'];
+    $pass = $_REQUEST['pass'];
+    $pass = md5($pass);
+
+    $query = $mysqli->query("SELECT * FROM users WHERE 'email' = `$email` AND 'pass' = `$pass`")
+
+    
+    if($query != '') {
+        $query = "INSERT INTO users VALUES (null, `$name`, `$email`, `$pass`)";
+    }
+
+    if(mysqli_query($mysqli, $query)){
+        echo "Записи успешно добавлены.";
+    } else{
+        "ERROR: Не удалось выполнить $query. " . mysqli_error($mysqli);
+    }
+
+    $mysqli->close();
+
+?>
+
+<?php
+
+$mysqli = new mysqli('localhost', 'dan', '1234', 'anime-blog');
+
+if(mysqli_connect_errno()) {
+    printf('Соединение не установлено', mysqli_error());
+    exit();
+}
+
+
+$mysqli->set_charset('utf8');
+
+    $query = $mysqli->query("SELECT * FROM `users`");
+
+    $name = $_REQUEST['name'];
+    $email = $_REQUEST['email'];
+    $pass = $_REQUEST['pass'];
+    $pass = md5($pass);
+
+    if($query != '') {
+        $query = "INSERT INTO users VALUES (null, `$name`, `$email`, `$pass`)";
+    }
+
+    if(mysqli_query($mysqli, $query)){
+        echo "Записи успешно добавлены.";
+    } else{
+        echo "ERROR: Не удалось выполнить $query. " . mysqli_error($mysqli);
+    }
+
+    $mysqli->close();
 
 ?>
 <!DOCTYPE html>
