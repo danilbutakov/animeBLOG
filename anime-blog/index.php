f8371aba395480d77dfa881217a88b7df5c0ff85
danilbutakov <12021109359@irgups.ru>
2022-04-28 21:59:08 +0800

    Переделал пути php9

--- anime-blog/index.php
+++ anime-blog/index.php
@@ -10,7 +10,21 @@
 
     $mysqli->set_charset('utf8');
 
-    
+    $query = $mysqli->query('SELECT * FROM subscribe');
+
+    $emailSub = $_REQUEST['email__sub'];
+    if($query != '') {
+        $query = "INSERT INTO subscribe VALUES (null, '$emailSub')";
+    }
+
+
+    if(mysqli_query($mysqli, $query)){
+        echo "Записи успешно добавлены.";
+    } else{
+        "ERROR: Не удалось выполнить $query. " . mysqli_error($mysqli);
+    }
+
+    $mysqli(close);
 
 ?>
 <!DOCTYPE html>
