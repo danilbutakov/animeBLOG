181dbaef496109ce527cfdc7cfef36eea1b12572
danilbutakov <12021109359@irgups.ru>
2022-04-28 21:56:05 +0800

    Переделал пути php6

--- anime-blog/login.php
+++ anime-blog/login.php
@@ -1,6 +1,5 @@
 <?php
 
-    require_once __DIR__ . '/php/mysqlauthentication.php';
     require_once __DIR__ . '/php/authorization.php';
     require_once __DIR__ . '/php/registration.php';
 
--- php/subscribe.php
+++ php/subscribe.php
@@ -1,21 +1,29 @@
 <?php
 
-    echo __DIR__;
+$mysqli = new mysqli('localhost', 'dan', '1234', 'anime-blog');
 
-    // $query = $mysqli->query('SELECT * FROM subscribe');
-
-    // $emailSub = $_REQUEST['email__sub'];
-    // if($query != '') {
-    //     $query = "INSERT INTO subscribe VALUES (null, '$emailSub')";
-    // }
+if(mysqli_connect_errno()) {
+    printf('Соединение не установлено', mysqli_error());
+    exit();
+}
 
 
-    // if(mysqli_query($mysqli, $query)){
-        //echo "Записи успешно добавлены.";
-    // } else{
-    //     "ERROR: Не удалось выполнить $query. " . mysqli_error($mysqli);
-    // }
+$mysqli->set_charset('utf8');
 
-    // $mysqli(close);
+    $query = $mysqli->query('SELECT * FROM subscribe');
+
+    $emailSub = $_REQUEST['email__sub'];
+    if($query != '') {
+        $query = "INSERT INTO subscribe VALUES (null, '$emailSub')";
+    }
+
+
+    if(mysqli_query($mysqli, $query)){
+        echo "Записи успешно добавлены.";
+    } else{
+        "ERROR: Не удалось выполнить $query. " . mysqli_error($mysqli);
+    }
+
+    $mysqli(close);
 
 ?>
