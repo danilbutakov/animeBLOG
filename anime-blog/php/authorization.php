be20ad9a0453218c33451785242247b4b879eee9
danilbutakov <12021109359@irgups.ru>
2022-04-29 14:52:11 +0800

    Форма регистрации php10

--- anime-blog/php/authorization.php
+++ anime-blog/php/authorization.php
@@ -1,14 +1,25 @@
 <?php
 
+    session_start();
+
     $email = $_REQUEST['email'];
     $pass = $_REQUEST['pass'];
     $pass = md5($pass);
 
-    $query = $mysqli->query("SELECT * FROM users WHERE 'email' = '$email' AND 'pass' = '$pass'");
+    $query = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email' AND `pass` = '$pass'");
 
     
-    if($query != '') {
-        $query = "INSERT INTO users VALUES (null, '$email', '$pass')";
+    if(mysqli_num_rows($query) > 0) {
+        $user = mysqli_fetch_assoc($query);
+        
+        $_SESSION['user'] = [
+            "id" => $user['id'],
+            "name" => $user['name'],
+            "email" => $user['email']
+        ];
+    } else {
+        $_SESSION['message'] = 'Не верный логин или пароль';
+        header('Location: /login.php');
     }
 
     if(mysqli_query($mysqli, $query)){
