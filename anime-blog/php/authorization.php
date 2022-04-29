43f153bfddfdc2a41efc80e1bcc03b77799dd3fe
Dan <12021109359@irgups.ru>
2022-04-29 18:29:59 +0800

    Доделываю авторизацию1

--- anime-blog/php/authorization.php
+++ anime-blog/php/authorization.php
@@ -2,13 +2,12 @@
 
     session_start();
 
-    $check_user = $mysqli->query("SELECT * FROM users WHERE 'email' = '$email' AND 'pass' = '$pass'");
-
-    echo mysqli_num_rows($check_user);
-
     $email = $_REQUEST['email'];
     $pass = $_REQUEST['pass'];
     $pass = md5($pass);
 
+    $check_user = $mysqli->query("SELECT * FROM users WHERE 'email' = '$email' AND 'pass' = '$pass'");
+
+    echo mysqli_num_rows($check_user);
 
 ?>
