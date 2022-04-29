f0ce510b23118151113b631453694e410d85d874
danilbutakov <12021109359@irgups.ru>
2022-04-30 00:10:55 +0800

    Работа с авторизацией20

--- anime-blog/php/registration.php
+++ anime-blog/php/registration.php
@@ -12,7 +12,7 @@ if ($query != '') {
 }
 
 if (mysqli_query($mysqli, $query)) {
-    header('Location: /index.php');
+    header('Location: index.php');
 } else {
     //echo "ERROR: Не удалось выполнить $query. " . mysqli_error($mysqli);
 }
