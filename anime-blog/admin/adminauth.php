a5a85e1718ad2e5a719f05120bbb2d2686864a15
danilbutakov <12021109359@irgups.ru>
2022-04-30 21:39:07 +0800

    Работа с админкой1

--- anime-blog/admin/adminauth.php
+++ anime-blog/admin/adminauth.php
@@ -10,6 +10,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adminka</title>
     <link rel="stylesheet" href="/css/admin.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
