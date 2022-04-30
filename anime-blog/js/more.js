4485c3bf56b6c6396208b58711a977d7cfc6693f
danilbutakov <12021109359@irgups.ru>
2022-05-01 00:27:56 +0800

    Работа с разверткой19

--- anime-blog/js/more.js
+++ anime-blog/js/more.js
@@ -5,6 +5,10 @@ function readMore() {
     let more = document.getElementById("more");
     let btn = document.getElementById("btn2");
 
+    let dots2 = document.getElementById("dots2");
+    let more2 = document.getElementById("more2");
+    let btn2 = document.getElementById("btn3");
+
     if(dots.style.display === "none") {
         dots.style.display="inline";
         btn.innerHTML="Подробнее";
