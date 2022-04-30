693c1e692ac54aac53f2e81b8310842246dd0e06
danilbutakov <12021109359@irgups.ru>
2022-04-30 15:14:19 +0800

    Подключение admin.php30

--- anime-blog/admin/admin.php
+++ anime-blog/admin/admin.php
@@ -1,15 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-</head>
-
-<body>
-    <p>Привет <?= $_COOKIE['user'] ?>. Чтобы выйти нажмите <a href="admin/exitadmin.php">здесь</a></p>
-</body>
-
-</html>
+<?php
+echo "Вы в админке";
