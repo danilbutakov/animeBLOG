5bbdadace762612d3150cd7ab593b597f18c9ae3
danilbutakov <12021109359@irgups.ru>
2022-04-30 14:53:16 +0800

    Подключение admin.php18

--- anime-blog/admin/admin.php
+++ anime-blog/admin/admin.php
@@ -1,3 +1,15 @@
-<?php
-session_start();
-echo "Вы в админке";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+
+<body>
+    <p>Привет <?= $_COOKIE['user'] ?>. Чтобы выйти нажмите <a href="admin/exitadmin.php">здесь</a></p>
+</body>
+
+</html>
--- anime-blog/admin/logadmin.php
+++ anime-blog/admin/logadmin.php
@@ -8,11 +8,11 @@ $result = $mysqli->query("SELECT * FROM `admin` WHERE `login` = '$login' AND `pa
 
 $user = $result->fetch_assoc();
 
-print_r($user);
+if (count($user) == 0) {
+    echo "Такой пользователь не найден";
+    exit();
+} else {
+    header('Location: /admin.php');
+}
 
-// if ($array) {
-//     echo "Такой пользователь не найден";
-//     exit();
-// } else {
-//     header('Location: /admin.php');
-// }
+setcookie('user', $user['email'], time() + 3600, "admin/admin.php");
