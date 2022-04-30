b09a981849734b4d03eabc8cec1ef820c1822e0a
danilbutakov <12021109359@irgups.ru>
2022-04-30 18:49:47 +0800

    Разделение частей сайта на файлы1

---
+++ anime-blog/public/manga.php
@@ -0,0 +1,78 @@
+<div class="blog__manga">
+    <div class="manga__title">Manga reads</div>
+    <div class="manga__item2">
+        <a href="" class="item3__link3">
+            <img src="img/home/akame-small (2).png" alt="">
+        </a>
+        <div class="item3__info3">
+            <div class="item2__title2">
+                <a href="">
+                    <h4>Akame Ga Kill: Season finale</h4>
+                </a>
+            </div>
+            <div class="item3__text3">
+                21 march 2021
+            </div>
+        </div>
+    </div>
+    <div class="manga__item2">
+        <a href="" class="item3__link3">
+            <img src="img/home/naruto-small.png" alt="">
+        </a>
+        <div class="item3__info3">
+            <div class="item2__title2">
+                <a href="">
+                    <h4>Naruto Uzumaki: Hidden Village</h4>
+                </a>
+            </div>
+            <div class="item3__text3">
+                21 march 2021
+            </div>
+        </div>
+    </div>
+    <div class="manga__item2">
+        <a href="" class="item3__link3">
+            <img src="img/home/love-small.png" alt="">
+        </a>
+        <div class="item3__info3">
+            <div class="item2__title2">
+                <a href="">
+                    <h4>Love juice Season Priemere</h4>
+                </a>
+            </div>
+            <div class="item3__text3">
+                21 march 2021
+            </div>
+        </div>
+    </div>
+    <div class="manga__item2">
+        <a href="" class="item3__link3">
+            <img src="img/home/love-small.png" alt="">
+        </a>
+        <div class="item3__info3">
+            <div class="item2__title2">
+                <a href="">
+                    <h4>Love juice Season Priemere</h4>
+                </a>
+            </div>
+            <div class="item3__text3">
+                21 march 2021
+            </div>
+        </div>
+    </div>
+    <div class="manga__item2">
+        <a href="" class="item3__link3">
+            <img src="img/home/love-small.png" alt="">
+        </a>
+        <div class="item3__info3">
+            <div class="item2__title2">
+                <a href="">
+                    <h4>Love juice Season Priemere</h4>
+                </a>
+            </div>
+            <div class="item3__text3">
+                21 march 2021
+            </div>
+        </div>
+    </div>
+</div>
---
+++ anime-blog/public/new.php
@@ -0,0 +1,47 @@
+<section class="new">
+    <div class="new__content _container">
+        <h3 class="new__title title">New & Trendly</h3>
+        <div class="new__rows">
+            <div class="new__row">
+                <div class="row__items">
+                    <a href="" class="row__img">
+                        <img src="img/home/Rectangle 39.png" alt="">
+                    </a>
+                    <div class="row__item">
+                        <div class="row__info">Fantasy <span>· 1 Month Ago</span></div>
+                        <div class="row__title"><a href="">Akame Ga Kill: Season finale</a></div>
+                        <div class="row__text">The CSS Grid Layout Module offers a <br> grid-based layout system, with rows and <br> columns, making it easier to design web pages <br> without having to use floats and positioning.</div>
+                        <div class="row__under under">
+                            <div class="under__read">
+                                <span>12 Min Read</span>
+                            </div>
+                            <div class="under__full">
+                                <a href="attack-on-titans.html"><span>Read Full <img src="img/ico/arrow.png" alt=""></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="new__row">
+                <div class="row__items">
+                    <div class="row__item">
+                        <div class="row__info">Fantasy <span>· 1 Month Ago</span></div>
+                        <div class="row__title"><a href="attack-on-titans.html">Attack On Titans</a></div>
+                        <div class="row__text">The CSS Grid Layout Module offers a <br> grid-based layout system, with rows and <br> columns, making it easier to design web pages <br> without having to use floats and positioning.</div>
+                        <div class="row__under under">
+                            <div class="under__read">
+                                <span>12 Min Read</span>
+                            </div>
+                            <div class="under__full">
+                                <a href="attack-on-titans.html"><span>Read Full <img src="img/ico/arrow.png" alt=""></span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="attack-on-titans.html" class="row__img">
+                        <img src="img/home/Rectangle 42.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
