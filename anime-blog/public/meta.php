fac7e9d172c6afd7e23a5b67b43b58fd66a50212
danilbutakov <12021109359@irgups.ru>
2022-04-30 20:09:18 +0800

    Разделение частей сайта на файлы23

--- anime-blog/public/meta.php
+++ anime-blog/public/meta.php
@@ -1,6 +1,6 @@
 <?php
-$sql2 = $pdo->query("SELECT * FROM meta");
-$sql2->execute();
-$res2 = $sql2->fetch(PDO::FETCH_ASSOC);
+// $sql2 = $pdo->query("SELECT * FROM meta");
+// $sql2->execute();
+// $res2 = $sql2->fetch(PDO::FETCH_ASSOC);
 ?>
 <meta name="<?php echo $res2['title'] ?>" content="<?php echo $res2['description'] ?>">
