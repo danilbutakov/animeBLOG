fade0c039ea8690e45a4975ddc11339010e3aedf
danilbutakov <12021109359@irgups.ru>
2022-04-30 23:12:03 +0800

    Работа с админкой55

--- anime-blog/admin/new.php
+++ anime-blog/admin/new.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
+$mysqli = new mysqli('localhost', 'dan', '1234', 'anime-blog');
 
+if (mysqli_connect_errno()) {
+    printf('Соединение не установлено');
+    exit();
+}
 print_r($mysqli);
 
 ?>
