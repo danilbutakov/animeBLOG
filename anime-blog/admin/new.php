fc36445d818881f7e009d86809b6eba9583cc7cd
danilbutakov <12021109359@irgups.ru>
2022-04-30 22:19:49 +0800

    Работа с админкой14

--- anime-blog/admin/new.php
+++ anime-blog/admin/new.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-$mysqli = new mysqli('localhost', 'dan', '1234', 'anime-blog');
 
 ?>
 <!DOCTYPE html>
@@ -16,27 +15,28 @@ $mysqli = new mysqli('localhost', 'dan', '1234', 'anime-blog');
 </head>
 
 <body>
-    <div>
+    <div style="text-align: center;">
         <h1>Редактирование контактной информации</h1>
         <?php if (!empty($_SESSION['login'])) : ?>
 
             <?php echo "Добрый день, " . $_SESSION['login']; ?><br>
             <a href="logout.php">Выйти</a><br>
-
+            <?php $sql = $mysqli->query("SELECT * FROM `new`");
+            $sql->execute();
+            $res2 = $sql->fetch_assoc();
+            ?>
+            <form action="" method="post">
+                <input type="text" name="info_genre" value="<?php echo $res->info_genre ?>">
+                <input type="text" name="info_date" value="<?php echo $res->info_date ?>">
+                <input type="text" name="title" value="<?php echo $res->title ?>">
+                <input type="text" name="description" value="<?php echo $res->description ?>">
+                <input type="text" name="read_info" value="<?php echo $res->read_info ?>">
+            </form>
         <?php else :
             echo '<h2>Вы что хакер?</h2>';
             echo '<a href="/../admin.php">На главную</a>';
         ?>
         <?php endif ?>
-        <?php $sql = $mysqli->query("SELECT * FROM `new`");
-        $res2 = $sql->fetch_assoc(); ?>
-        <form action="" method="post">
-            <input type="text" name="info_genre" value="<?php echo $res->info_genre ?>">
-            <input type="text" name="info_date" value="<?php echo $res->info_date ?>">
-            <input type="text" name="title" value="<?php echo $res->title ?>">
-            <input type="text" name="description" value="<?php echo $res->description ?>">
-            <input type="text" name="read_info" value="<?php echo $res->read_info ?>">
-        </form>
     </div>
 </body>
 
