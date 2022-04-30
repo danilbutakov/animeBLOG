129d06bc69733611536ca0f349024b9ff5a6de77
danilbutakov <12021109359@irgups.ru>
2022-04-30 13:38:57 +0800

    Стилизация admin.php2

--- anime-blog/adminka/admin.php
+++ anime-blog/adminka/admin.php
@@ -6,10 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
+    <link rel="stylesheet" href="/anime-blog/css/admin.css">
 </head>
 
 <body>
-    <h2>Вход в административную панель</h2>
+    <h2 class="title">Вход в административную панель</h2>
+    <form action="" method="post">
+        <div class="form-group">
+            <input type="text" placeholder="Введите логин">
+        </div>
+        <div class="form-group">
+            <input type="password" placeholder="Введите пароль">
+        </div>
+        <button class="btn">Войти</button>
+    </form>
 </body>
 
 </html>
