c9c6d401a7d141eff3364cb92587f25ab6f41bf0
danilbutakov <12021109359@irgups.ru>
2022-04-30 22:00:33 +0800

    Работа с админкой11

--- anime-blog/admin/new.php
+++ anime-blog/admin/new.php
@@ -26,22 +26,22 @@ if (mysqli_connect_errno()) {
 
             <?php echo "Добрый день, " . $_SESSION['login']; ?><br>
             <a href="logout.php">Выйти</a><br>
-            <?php $sql = $mysqli->query("SELECT * FROM `new`");
-            $sql->execute();
-            $res2 = $sql->fetch_assoc();
-            ?>
-            <form action="" method="post">
-                <input type="text" name="info_genre" value="<?php echo $res->info_genre ?>">
-                <input type="text" name="info_date" value="<?php echo $res->info_date ?>">
-                <input type="text" name="title" value="<?php echo $res->title ?>">
-                <input type="text" name="description" value="<?php echo $res->description ?>">
-                <input type="text" name="read_info" value="<?php echo $res->read_info ?>">
-            </form>
+
         <?php else :
             echo '<h2>Вы что хакер?</h2>';
             echo '<a href="/../admin.php">На главную</a>';
         ?>
         <?php endif ?>
+        <?php $sql = $mysqli->query("SELECT * FROM `new`");
+        $sql->execute();
+        $res2 = $sql->fetch_assoc(); ?>
+        <form action="" method="post">
+            <input type="text" name="info_genre" value="<?php echo $res->info_genre ?>">
+            <input type="text" name="info_date" value="<?php echo $res->info_date ?>">
+            <input type="text" name="title" value="<?php echo $res->title ?>">
+            <input type="text" name="description" value="<?php echo $res->description ?>">
+            <input type="text" name="read_info" value="<?php echo $res->read_info ?>">
+        </form>
     </div>
 </body>
 
