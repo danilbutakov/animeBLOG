93e016d2cfd8afd813233c56a0d8dccc7727f403
danilbutakov <12021109359@irgups.ru>
2022-04-30 18:44:50 +0800

    Разделение частей сайта на файлы

--- anime-blog/index.php
+++ anime-blog/index.php
@@ -31,26 +31,7 @@ require_once __DIR__ . '/php/registration.php';
 
 <body>
     <div class="wrapper">
-        <header class="header">
-            <div class="header__container _container">
-                <div class="header__body">
-                    <a href="index.php" class="header__logo">
-                        <img src="img/header/Logo.png" alt="Logo">
-                    </a>
-                    <div class="header__burger">
-                        <span></span>
-                    </div>
-                    <nav class="header__menu">
-                        <ul class="header__list">
-                            <li><a href="index.php" class="header__link _active">Home</a></li>
-                            <li><a href="" class="header__link">Connect</a></li>
-                            <li><a href="#" class="header__link sign-up" id="open__pop_up">Sign up</a></li>
-                            <li><a href="login.php" class="header__link login">Login</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </header>
+        <?php require_once __DIR__ . '/public/header.php'; ?>
         <main class="page">
             <section class="welcome">
                 <div class="welcome__content">
