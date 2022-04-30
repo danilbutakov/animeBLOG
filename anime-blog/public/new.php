57dd0e8afa66f3f0581189e11561b7135bd140df
danilbutakov <12021109359@irgups.ru>
2022-04-30 19:28:47 +0800

    Разделение частей сайта на файлы8

--- anime-blog/public/new.php
+++ anime-blog/public/new.php
@@ -1,3 +1,9 @@
+<?php
+require_once __DIR__ . '/php/mysqlauthentication.php';
+$query = $mysqli->query("SELECT * FROM new");
+$query->execute();
+$res = $query->fetch_assoc();
+?>
 <section class="new">
     <div class="new__content _container">
         <h3 class="new__title title">New & Trendly</h3>
@@ -8,12 +14,12 @@
                         <img src="img/home/Rectangle 39.png" alt="">
                     </a>
                     <div class="row__item">
-                        <div class="row__info">Fantasy <span>· 1 Month Ago</span></div>
-                        <div class="row__title"><a href="">Akame Ga Kill: Season finale</a></div>
-                        <div class="row__text">The CSS Grid Layout Module offers a <br> grid-based layout system, with rows and <br> columns, making it easier to design web pages <br> without having to use floats and positioning.</div>
+                        <div class="row__info"><?php echo $res['info_genre'] ?> <span>· <?php echo $res['info_date'] ?></span></div>
+                        <div class="row__title"><a href=""><?php echo $res['title'] ?></a></div>
+                        <div class="row__text"><?php echo $res['description'] ?></div>
                         <div class="row__under under">
                             <div class="under__read">
-                                <span>12 Min Read</span>
+                                <span><?php echo $res['read_info'] ?></span>
                             </div>
                             <div class="under__full">
                                 <a href="attack-on-titans.php"><span>Read Full <img src="img/ico/arrow.png" alt=""></span></a>
