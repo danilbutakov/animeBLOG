a8af156a8922caef66df3615717fc5fcfc94e75e
danilbutakov <12021109359@irgups.ru>
2022-04-30 22:26:46 +0800

    Работа с админкой25

--- anime-blog/php/mysqlauthentication.php
+++ anime-blog/php/mysqlauthentication.php
@@ -1,9 +1,5 @@
 <?php
 $mysqli = new mysqli('localhost', 'dan', '1234', 'anime-blog');
 
-if (mysqli_connect_errno()) {
-    printf('Соединение не установлено', mysqli_error());
-    exit();
-}
 
 $mysqli->set_charset('utf8');
