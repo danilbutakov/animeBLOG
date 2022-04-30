b27b886784b9ba6f5815057709c7b01d7e810141
danilbutakov <12021109359@irgups.ru>
2022-04-30 15:09:32 +0800

    Подключение admin.php25

--- anime-blog/admin/logadmin.php
+++ anime-blog/admin/logadmin.php
@@ -8,9 +8,11 @@ $result = $mysqli->query("SELECT * FROM `admin` WHERE `login` = '$login' AND `pa
 $user = $result->fetch_assoc();
 
 if ($user['id'] > 0) {
-    //header('Location: /admin/admin.php');
+    header('Location: /admin/admin.php');
+    exit();
 } else {
-    //header('Location: /admin.php');
+    header('Location: /admin.php');
+    exit();
 }
 
 setcookie('user', $user['email'], time() + 3600, "admin/admin.php");
