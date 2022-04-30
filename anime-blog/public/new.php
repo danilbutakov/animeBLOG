41955e4d9bebb25fd280e28f007ec4f3bf1e16e3
danilbutakov <12021109359@irgups.ru>
2022-04-30 19:34:24 +0800

    Разделение частей сайта на файлы13

--- anime-blog/public/new.php
+++ anime-blog/public/new.php
@@ -1,9 +1,8 @@
 <?php
 require_once __DIR__ . '/php/mysqlauthentication.php';
-$query = $mysqli->query("SELECT * FROM `new`");
+$query = $mysqli->query("SELECT * FROM new");
 $query->execute();
-$res = $query->fetch(PDO::FETCH_ASSOC);
-print_r($res);
+$res = $query->fetch_assoc();
 ?>
 <section class="new">
     <div class="new__content _container">
@@ -15,12 +14,12 @@ print_r($res);
                         <img src="img/home/Rectangle 39.png" alt="">
                     </a>
                     <div class="row__item">
-                        <div class="row__info"><?php echo $res["info_genre"] ?> <span>· <?php echo $res["info_date"] ?></span></div>
-                        <div class="row__title"><a href=""><?php echo $res["title"] ?></a></div>
-                        <div class="row__text"><?php echo $res["description"] ?></div>
+                        <div class="row__info"> <span>· 1 Month Ago</span></div>
+                        <div class="row__title"><a href="">Akame Ga Kill: Season finale</a></div>
+                        <div class="row__text">The CSS Grid Layout Module offers a <br> grid-based layout system, with rows and <br> columns, making it easier to design web pages <br> without having to use floats and positioning.</div>
                         <div class="row__under under">
                             <div class="under__read">
-                                <span><?php echo $res["read_info"] ?></span>
+                                <span>12 Min Read</span>
                             </div>
                             <div class="under__full">
                                 <a href="attack-on-titans.php"><span>Read Full <img src="img/ico/arrow.png" alt=""></span></a>
