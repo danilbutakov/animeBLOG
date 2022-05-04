caa8a723681c28b3598eed8468cb284f82e7844d
danilbutakov <12021109359@irgups.ru>
2022-05-04 13:32:32 +0800

    Добавил апдейт в new

--- anime-blog/public/new.php
+++ anime-blog/public/new.php
@@ -5,11 +5,10 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 ?>
 
 <?php
-	$qur="SELECT * FROM new;";
-	$result = $conn->query("SELECT * FROM new");
-	$res = mysqli_fetch_assoc($row);
-	foreach ($result as $rows){
-	
+$qur = "SELECT * FROM new;";
+$result = $conn->query("SELECT * FROM new");
+$res = mysqli_fetch_assoc($row);
+foreach ($result as $rows) {
 }
 ?>
 <section class="new">
@@ -22,12 +21,12 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
                         <img src="img/home/Rectangle 39.png" alt="">
                     </a>
                     <div class="row__item">
-                        <div class="row__info"><span><?= $rows['info_genre'];?></span> <span>· <?= $rows['info_date']; ?></span></div>
-                        <div class="row__title"><a href="">Akame Ga Kill: Season finale</a></div>
-                        <div class="row__text">The CSS Grid Layout Module offers a <br> grid-based layout system, with rows and <br> columns, making it easier to design web pages <br> without having to use floats and positioning.</div>
+                        <div class="row__info"><span><?= $rows['info_genre']; ?></span> <span>· <?= $rows['info_date']; ?></span></div>
+                        <div class="row__title"><a href="#"><span><?= $rows['title']; ?></span></a></div>
+                        <div class="row__text"><span><?= $rows['descr']; ?></span></div>
                         <div class="row__under under">
                             <div class="under__read">
-                                <span>12 Min Read</span>
+                                <span><span><?= $rows['read_info']; ?></span></span>
                             </div>
                             <div class="under__full">
                                 <a href="attack-on-titans.php"><span>Read Full <img src="img/ico/arrow.png" alt=""></span></a>
