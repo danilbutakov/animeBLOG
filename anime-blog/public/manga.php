4e70b841ea56a28ce5b7ae799f06583e22f8b31d
danilbutakov <12021109359@irgups.ru>
2022-05-04 14:10:31 +0800

    Работа с manga1

--- anime-blog/public/manga.php
+++ anime-blog/public/manga.php
@@ -1,5 +1,5 @@
 <div class="blog__manga">
-    <div class="manga__title">Manga reads</div>
+    <div class="manga__title"><span><?= $rows['head']; ?></span></div>
     <div class="manga__item2">
         <a href="" class="item3__link3">
             <img src="img/home/akame-small (2).png" alt="">
@@ -7,11 +7,11 @@
         <div class="item3__info3">
             <div class="item2__title2">
                 <a href="">
-                    <h4>Akame Ga Kill: Season finale</h4>
+                    <h4><span><?= $rows['title']; ?></span></h4>
                 </a>
             </div>
             <div class="item3__text3">
-                21 march 2021
+                <span><?= $rows['info']; ?></span>
             </div>
         </div>
     </div>
