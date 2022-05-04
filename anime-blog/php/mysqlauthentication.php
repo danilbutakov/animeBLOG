e6da0794ecfdfa8c32b9d8c67734c75a8e05f5c9
danilbutakov <12021109359@irgups.ru>
2022-05-04 13:29:23 +0800

    Убрал div в подключении бд

--- anime-blog/php/mysqlauthentication.php
+++ anime-blog/php/mysqlauthentication.php
@@ -1,9 +1,9 @@
 <?php
 $mysqli = new mysqli('localhost', 'dan', '1234', 'anime-blog');
-$conn = mysqli_connect("localhost", "dan" , "1234", "anime-blog");
+$conn = mysqli_connect("localhost", "dan", "1234", "anime-blog");
 if (mysqli_connect_errno()) {
     printf('Соединение не установлено');
     exit();
 }
-echo "<div style='position:absolute;top:20%;left:20%;z-index:999999999999;'>connect succesfull</div>";
+
 $mysqli->set_charset('utf8');
