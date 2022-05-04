1a9b855c4390ddd348a9484c3b047695f769b1aa
danilbutakov <12021109359@irgups.ru>
2022-05-04 13:44:14 +0800

    Стилизация admin2

--- anime-blog/admin/adminauth.php
+++ anime-blog/admin/adminauth.php
@@ -18,11 +18,13 @@ session_start();
 
         <?php if (!empty($_SESSION['login'])) : ?>
 
-            <?php echo "Добрый день, " . $_SESSION['login']; ?><br>
-            <a href="logout.php" style="padding: 10px 0px;">Выйти</a><br>
-            <a href="new.php">New</a>
-            <a href="#">Trending</a>
-            <a href="manga.php">Manga</a>
+            <div class="hello"><?php echo "Добрый день, " . $_SESSION['login']; ?><br></div>
+            <div class="logout"><a href="logout.php" style="padding: 10px 0px;">Выйти</a><br></div>
+            <div class="section">
+                <a href="new.php">New</a>
+                <a href="#">Trending</a>
+                <a href="manga.php">Manga</a>
+            </div>
         <?php else :
             echo '<h2>Вы что хакер?</h2>';
             echo '<a href="/../admin.php">На главную</a>';
