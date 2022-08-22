514b58fcbcdb1c8381124c760370bcdfd4ed9f62
danilbutakov <12021109359@irgups.ru>
2022-08-22 19:57:30 +0800

    тест регистрации без пустых записей при обновлении6

--- anime-blog/php/registration.php
+++ anime-blog/php/registration.php
@@ -6,13 +6,13 @@ $pass = filter_var(trim($_POST['pass']));
 $pass = md5($pass);
 
 if (mb_strlen($name) < 3 || mb_strlen($name) > 50) {
-    echo "Недопустимая длина имени";
+    //echo "Недопустимая длина имени";
     exit();
 } else if (mb_strlen($email) < 5 || mb_strlen($email) > 50) {
-    echo "Недопустимая длина почты";
+    //echo "Недопустимая длина почты";
     exit();
 } else if (mb_strlen($pass) < 8 || mb_strlen($pass) > 100) {
-    echo "Недопустимая длина пароля";
+    //echo "Недопустимая длина пароля";
     exit();
 }
 
